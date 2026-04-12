8948e5787ae16659e492ab43b53bbf3dfca6a84d
Josemerlin2103 <josemerlin2103@gmail.com>
2026-04-12 17:35:32 +0530

    Added trace, screenshot and video config

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    
+  trace: 'on',
+  screenshot: 'on',
+  video: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -38,7 +41,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
- 
+ /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -48,7 +51,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-    
+    */
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',

e6acfb176b1a943962c2fb3128957b4d7548f0f2
Josemerlin2103 <josemerlin2103@gmail.com>
2026-04-24 07:59:10 +0530

    Day 3: Implemented Data-Driven testing and refactored ProductPage POM for dynamic product selection

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -2,9 +2,9 @@ import {Page} from '@playwright/test';
 export class ProductPage {
 
     constructor(private page:Page) {}
-    async openProduct(){
-    await this.page.getByRole('link', { name: 'Samsung galaxy s6' }).click();
-    }
+    async openProduct(name: string) {
+    await this.page.getByRole('link', { name: name }).click();
+}
     async addToCart(){
     await this.page.getByRole('link', { name: 'Add to cart' }).click();
     }
--- tests/addtocart.spec.ts
+++ tests/addtocart.spec.ts
@@ -1,24 +1,27 @@
 import { test, expect } from '@playwright/test';
 import { ProductPage } from '../pages/ProductPage';
+// @ts-ignore
+import testData from '../utils/testData.json';
 
-test('Add product to cart' ,async({ page }) =>{
-const product= new  ProductPage(page);
+for (const data of testData) {
+    test(`Add ${data.productName} to cart`, async ({ page }) => {
+        const product = new ProductPage(page);
 
-await page.goto('https://www.demoblaze.com/');
+        await page.goto('https://www.demoblaze.com/');
 
-page.on('dialog',async dialog =>{
-console.log(dialog.message());
-await dialog.accept();
-});
+        // 1. Open the  product from our JSON list
+       
+        await product.openProduct(data.productName)
 
-await product.openProduct();
-await product.addToCart();
+        // 2. Add to cart
+        page.once('dialog', async dialog => {
+            console.log(dialog.message());
+            await dialog.accept();
+        });
+        await product.addToCart();
 
-await page.waitForTimeout(2000);
-
-await product.openCart();
-
-
-
-await expect(page.locator('#tbodyid')).toContainText('Samsung galaxy s6', { ignoreCase: true });
-});
+        // 3. Verify the specific product is in the cart
+        await page.getByRole('link', { name: 'Cart', exact: true }).click();
+        await expect(page.locator('#tbodyid')).toContainText(data.productName);
+    });
+}

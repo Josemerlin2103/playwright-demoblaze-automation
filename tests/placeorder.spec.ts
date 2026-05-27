3b906bdba63aeb0ab2ac4b18cedccd8cf864a4fb
Josemerlin2103 <josemerlin2103@gmail.com>
2026-05-27 18:20:28 +0530

    Fix: correct import casing for Linux CI

--- tests/placeorder.spec.ts
+++ tests/placeorder.spec.ts
@@ -1,6 +1,6 @@
 import { test,expect } from '@playwright/test';
 import { ProductPage } from '../pages/ProductPage';
-import { OrderPage } from '../pages/orderpage';
+import { OrderPage } from '../pages/OrderPage';
 
 test ('place order',async({page})=>{
 
